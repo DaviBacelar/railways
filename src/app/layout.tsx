3b4b622f5990461a4c95412f6f1d56e1d4d77d7d
BACELAR Davi <davi_pexoto@hotmail.com>
2024-03-09 19:34:04 +0000

    [FIX]: remove header from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import React from "react";
-import Header from "@/components/Header";
 import CssBaseline from '@mui/material/CssBaseline';
 
 const inter = Inter({ subsets: ["latin"] });
